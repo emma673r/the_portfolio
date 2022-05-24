664c4cc920109c60f2c7ebf28276466086a7b25f
emma673r <90177139+emma673r@users.noreply.github.com>
2022-05-24 14:25:20 +0200

    start på design

---
+++ accordion.js
@@ -0,0 +1,3 @@
+// click events på billederen for at sende videre til det clicked project
+
+// single views ? (pop up ?), another html ? , different sections underneath and scroll mouvement to the project ?
