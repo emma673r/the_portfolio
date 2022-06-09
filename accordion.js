2d350afaf0caf8465457ac25a36ac9f3f152f1a3
emma673r <90177139+emma673r@users.noreply.github.com>
2022-06-09 18:12:41 +0200

    web på plads responsiv

--- accordion.js
+++
@@ -1,3 +0,0 @@
-// click events på billederen for at sende videre til det clicked project
-
-// single views ? (pop up ?), another html ? , different sections underneath and scroll mouvement to the project ?
